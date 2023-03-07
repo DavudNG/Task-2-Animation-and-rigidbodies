c1a54fab7204e03812943d11013be298d8b1f980
DavudNG <brokenarma@gmail.com>
2023-03-07 17:24:54 +1100

    Update
    Upload all the things

---
+++ Assets/Scripts/MeteorLogic.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool bIsLive = true;
+    public Vector3 vDirectVelocity;
+    public GameObject myExplosion;
+
+
+    void Start()
+    {
+        vDirectVelocity = new Vector3(0, -9.8f, -9);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(bIsLive)
+        {
+            this.GetComponent<Rigidbody>().velocity += vDirectVelocity;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("a collision occurred");
+        bIsLive = false;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        myExplosion.SetActive(true);
+        this.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
